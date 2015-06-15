5bec531ab2a0e166766ec0451738824c3bbb8c7c
benji6 <benhall22@hotmail.co.uk>
2015-06-15 12:08:41 +0100

    work on view

--- client/js/AnalysisView.jsx
+++ client/js/AnalysisView.jsx
@@ -23,85 +23,114 @@ export default () => {
         oxygenLevel,
       } = environment;
 
-      return <div>
-        <table className="table-condensed">
-          <thead>
-            <tr>
-              <th>High-Level Analysis</th>
-              <th>Output</th>
-              <th>Gobbler Analysis</th>
-              <th>Output</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Light Level</td>
-              <td>{lightLevel}</td>
-              <td>Average Energy</td>
-              <td>{(totalEnergy / totalGobblers).toFixed(2)}</td>
-            </tr>
-            <tr>
-              <td>Oxygen Level</td>
-              <td>{oxygenLevel.toFixed(0)}</td>
-              <td>Average Velocity Coefficient</td>
-              <td>{(totalVelocityCoefficient / totalGobblers).toFixed(2)}</td>
-            </tr>
-            <tr>
-              <td>Carbon Dioxide Level</td>
-              <td>{carbonDioxideLevel.toFixed(0)}</td>
-              <td>Average Attack Coefficient</td>
-              <td>{(totalAttackCoefficient / totalGobblers).toFixed(2)}</td>
-            </tr>
-            <tr>
-              <td>Total Energy</td>
-              <td>{totalEnergy.toFixed(0)}</td>
-              <td>Average Defence Coefficient</td>
-              <td>{(totalDefenceCoefficient / totalGobblers).toFixed(2)}</td>
-            </tr>
-            <tr>
-              <td>Number of Gobblers</td>
-              <td>{totalGobblers}</td>
-              <td>Average Photosynthesis Coefficient</td>
-              <td>{(totalPhotosynthesisCoefficient / totalGobblers).toFixed(2)}</td>
-            </tr>
-            <tr>
-              <td>Number of Eatings</td>
-              <td>{eatCount}</td>
-            </tr>
-            <tr>
-              <td>Reproduction Count</td>
-              <td>{reproductionCount}</td>
-            </tr>
-            <tr>
-              <td>Death Count</td>
-              <td>{deathCount}</td>
-            </tr>
-            <tr>
-              <td>Youngest Generation</td>
-              <td>{intYoungestGen}</td>
-            </tr>
-            <tr>
-              <td>Oldest Generation</td>
-              <td>{intOldestGen}</td>
-              <td className="right">Seconds Elapsed</td>
-              <td>{this.props.secondsElapsed}</td>
-            </tr>
-          </tbody>
-        </table>
-        <table>
-          <thead>
-            <tr>
-              <th>Movement Strategy Name</th>
-              <th>Percentage</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Object.keys(movementStrategies).sort().map((strategyName) => <tr>
-                <td>{strategyName}</td>
-                <td>{(movementStrategies[strategyName] / totalGobblers * 100).toFixed(0) + "%"}</td>
-              </tr>)}
-          </tbody>
-        </table>
+      return <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-1"></div>
+          <div className="col-sm-3">
+            <table className="table-condensed">
+              <thead>
+                <tr>
+                  <th>High-Level Analysis</th>
+                  <th>Value</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Light Level</td>
+                  <td>{lightLevel}</td>
+                </tr>
+                <tr>
+                  <td>Oxygen Level</td>
+                  <td>{oxygenLevel.toFixed(0)}</td>
+                </tr>
+                <tr>
+                  <td>Carbon Dioxide Level</td>
+                  <td>{carbonDioxideLevel.toFixed(0)}</td>
+                </tr>
+                <tr>
+                  <td>Total Energy</td>
+                  <td>{totalEnergy.toFixed(0)}</td>
+                </tr>
+                <tr>
+                  <td>Number of Gobblers</td>
+                  <td>{totalGobblers}</td>
+                </tr>
+                <tr>
+                  <td>Number of Eatings</td>
+                  <td>{eatCount}</td>
+                </tr>
+                <tr>
+                  <td>Reproduction Count</td>
+                  <td>{reproductionCount}</td>
+                </tr>
+                <tr>
+                  <td>Death Count</td>
+                  <td>{deathCount}</td>
+                </tr>
+                <tr>
+                  <td>Youngest Generation</td>
+                  <td>{intYoungestGen}</td>
+                </tr>
+                <tr>
+                  <td>Oldest Generation</td>
+                  <td>{intOldestGen}</td>
+                </tr>
+                <tr>
+                  <td>Seconds Elapsed</td>
+                  <td>{this.props.secondsElapsed}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="col-sm-3">
+            <table className="table-condensed">
+              <thead>
+                <tr>
+                  <th>Gobbler Analysis</th>
+                  <th>Output</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Average Energy</td>
+                  <td>{(totalEnergy / totalGobblers).toFixed(2)}</td>
+                </tr>
+                <tr>
+                  <td>Average Velocity Coefficient</td>
+                  <td>{(totalVelocityCoefficient / totalGobblers).toFixed(2)}</td>
+                </tr>
+                <tr>
+                  <td>Average Attack Coefficient</td>
+                  <td>{(totalAttackCoefficient / totalGobblers).toFixed(2)}</td>
+                </tr>
+                <tr>
+                  <td>Average Defence Coefficient</td>
+                  <td>{(totalDefenceCoefficient / totalGobblers).toFixed(2)}</td>
+                </tr>
+                <tr>
+                  <td>Average Photosynthesis Coefficient</td>
+                  <td>{(totalPhotosynthesisCoefficient / totalGobblers).toFixed(2)}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="col-sm-3">
+            <table className="table-condensed">
+              <thead>
+                <tr>
+                  <th>Movement Strategy Name</th>
+                  <th>Percentage</th>
+                </tr>
+              </thead>
+              <tbody>
+                {Object.keys(movementStrategies).sort().map((strategyName) => <tr>
+                    <td>{strategyName}</td>
+                    <td>{(movementStrategies[strategyName] / totalGobblers * 100).toFixed(0) + "%"}</td>
+                  </tr>)}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>;
     }
   });
